1dbec4723091556d64a45f9b1c115a36baa250df
Ruonans <156931806+Ruonans@users.noreply.github.com>
2024-02-11 05:17:08 -0500

    hw3-updated

---
+++ solution-hw3/js/detail.js
@@ -0,0 +1,45 @@
+
+const glazingOptions = {
+    'Keep original': 0.00,
+    'Sugar milk': 0.00,
+    'Vanilla milk': 0.50,
+    'Double chocolate': 1.50
+};
+  
+  const packOptions = {
+    '1': 1,
+    '3': 3,
+    '6': 5,
+    '12': 10
+};
+
+const basePrice = 2.49;
+let glazingChange =  glazingOptions['Keep original'];
+let packChange = packOptions['1'];
+displayPrice();
+
+function getGlazingP (){
+    let glazingOption = this.options[this.selectedIndex].text;
+    glazingChange = glazingOptions[glazingOption];
+    displayPrice();
+}
+function getPackP (){
+    let packOption = this.options[this.selectedIndex].text;
+    packChange = packOptions[packOption];
+    displayPrice();
+}
+
+
+function displayPrice(){
+    let finalPrice = document.querySelector('#price');
+    finalPrice.innerText = '$' + (Math.round((basePrice + glazingChange) * packChange * 100)/100).toFixed(2);
+}
+
+let selectGlazing = document.querySelector('#glazing-select');
+let selectPack = document.querySelector('#pack-select');
+
+selectGlazing.addEventListener('change', getGlazingP);
+selectPack.addEventListener('change', getPackP);
+
+
+
